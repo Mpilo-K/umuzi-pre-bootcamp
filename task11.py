c7936c0e6ac7c939c5e0d74e33f4da9743ef502e
Mpilo Khumalo <mpilozenzele0@gmail.com>
2020-12-06 14:32:32 +0200

    Made a few changes in each task as recommended by the reviewer

--- task11.py
+++ task11.py
@@ -2,11 +2,6 @@ def compare_fun():
     string1 = input("Write a word: ")
     string2 = input("Write a second word: ")
     common_letters = [character for character in string1 and string2 if character in string1 and string2]
-    #common_letters = []
-    #for character in string1 and string2:
-        #if character in string1 and string2:
-            #common_letters.append(character)
-            #print(character)
     string_of_letters = ', '.join(common_letters)
     return f'Common letters: {string_of_letters}'
 
--- task5.py
+++ task5.py
@@ -1,7 +1,7 @@
 def calculate_triangle_area(side1_length, side2_length, side3_length):
     semi_perimeter = 0.5 * (side1_length + side2_length + side3_length)
-    tringle_area = ((semi_perimeter*((semi_perimeter - side1_length)*(semi_perimeter - side2_length)*(semi_perimeter - side2_length))) ** 0.5)
+    tringle_area = ((semi_perimeter*((semi_perimeter - side1_length)*(semi_perimeter - side2_length)*(semi_perimeter - side3_length))) ** 0.5)
     return round(tringle_area, 2)
 
 
-print(calculate_triangle_area(7, 8, 9))
+print(calculate_triangle_area(3, 4, 5))
