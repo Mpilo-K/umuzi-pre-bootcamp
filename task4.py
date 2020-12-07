e18137b0057540a51e5375ff658a40e57302308a
Mpilo Khumalo <mpilozenzele0@gmail.com>
2020-12-07 12:22:36 +0200

    Made the kindly requested changes to tasks 4 and 8

--- task4.py
+++ task4.py
@@ -1,7 +1,8 @@
 def onenum3_and_sumhas3():
     num_1 = input('Enter the first number: ')
     num_2 = input('Enter the second number: ')
-    if (num_1 == str(3) or num_2 == (3)) and (str(3) in str((num_1 + num_2))):
+    sum_nums = str(num_1 + num_2)
+    if (num_1 == str(3) or num_2 == str(3)) and (str(3) in sum_nums):
             return 'True'
     else:
         return 'False'
--- task8.py
+++ task8.py
@@ -3,18 +3,16 @@ def number_to_time(number):
         hours = 0
     elif number >= 60:
         hours = round(number/60)
-    minutes = round(number % 60, 2)
     if hours <= 1:
         time1 = str(hours) + ' hour, '
     elif hours > 1:
         time1 = str(hours) + ' hours, '
+    minutes = round(number % 60, 2)
     if minutes <= 1:
         time2 = time1 + str(minutes) + ' minute'
     elif minutes > 1:
         time2 = time1 + str(minutes) + ' minutes'
-        # return f"{hours} hours, {minutes} minutes"
     return time2
-
 
 
 print(number_to_time(33))
